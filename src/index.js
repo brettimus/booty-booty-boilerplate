3134f5c8a3c1e27ca28619473b2a4ae428bf754d
brettimus <brbeut@gmail.com>
2015-12-28 22:40:58 -0800

    Add reacty boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Yo");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const App = () => ({
+    render() {
+        return (
+            <p style={{textAlign: "center", position: "absolute", top: "20%", width: "100%"}}>
+                <b>Hello!</b> And welcome to hot-boilerplate.
+            </p>
+        );
+    },
+});
+
+const appMount = document.getElementById("tada");
+
+global.RD = ReactDOM;
+
+ReactDOM.render(<App />, appMount);
